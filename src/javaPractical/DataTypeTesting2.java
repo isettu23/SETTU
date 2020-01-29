0088fc8b6c741ef152e4b2dccd01760e5a87a986
isettu23 <isettu23@gmail.com>
2020-01-29 06:44:13 +0530

    Second commit

---
+++ src/javaPractical/DataTypeTesting2.java
@@ -0,0 +1,39 @@
+package javaPractical;
+
+public class DataTypeTesting2 {
+
+	public static void main(String[] args) {
+		
+		byte age = 88;
+		System.out.println("the byte size is 2 digits");
+		System.out.println(age);
+		
+		short atmpin = 4444;
+		System.out.println("the short size is 4 digits");
+		System.out.println(atmpin);
+		
+		int regno = 917636372;
+		System.out.println("the int size is 9 digits");
+		System.out.println(regno);
+		
+		long phno = 9176363729176363729l;
+		System.out.println("the long size is 10-16 digits");
+		System.out.println(phno);
+
+		float sal = 23423.00f;
+		System.out.println(sal);
+		
+		double accno = 917636372917917636372917917636372917917636372917917636372917d;
+		System.out.println(accno);
+		
+		char c = '*';
+		System.out.println(c);
+		
+		boolean status = true;
+		System.out.println(status);
+
+		String str = "any text";
+		System.out.println(str);
+
+	}
+}
